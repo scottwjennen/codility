6e84c4673e1f4f5372eb01ced8f99842d813f98e
elementarysyntax <elementarysyntax@gmail.com>
2023-11-07 14:26:22 -0600

    Initializes project.

---
+++ Codility_Solutions/Codility_Solutions/BinaryGapRecursion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility_Solutions
+{
+    public static class BinaryGapRecursion
+    {
+        public static int SolutionMethod(int N)
+        {
+
+        }
+    }
+}
